2609f4093fbcd4cf141ffc48afa2e1f83e22e36d
Lucas Godoy <lcsdegodoy@outlook.com>
2019-12-18 12:33:59 -0300

    feat: add cors middleware

--- docs/restapi/configure_easypoll.go
+++ docs/restapi/configure_easypoll.go
@@ -9,6 +9,7 @@ import (
 	errors "github.com/go-openapi/errors"
 	runtime "github.com/go-openapi/runtime"
 	middleware "github.com/go-openapi/runtime/middleware"
+	"github.com/rs/cors"
 
 	"github.com/wunari/easypoll-backend/docs/restapi/operations"
 	"github.com/wunari/easypoll-backend/docs/restapi/operations/poll"
@@ -71,5 +72,6 @@ func setupMiddlewares(handler http.Handler) http.Handler {
 // The middleware configuration happens before anything, this middleware also applies to serving the swagger.json document.
 // So this is a good place to plug in a panic handling middleware, logging and metrics
 func setupGlobalMiddleware(handler http.Handler) http.Handler {
-	return handler
+	handleCORS := cors.AllowAll().Handler
+	return handleCORS(handler)
 }
